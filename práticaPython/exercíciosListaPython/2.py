2efa7865e4bdd40b477ed4bcbe156b8685bce11c
AThees <alvaro@thees.com.br>
2021-05-25 12:03:49 -0300

    Update 2.py

--- práticaPython/exercíciosListaPython/2.py
+++ práticaPython/exercíciosListaPython/2.py
@@ -2,16 +2,10 @@
 # parâmetro o salário atual do funcionário e a porcentagem de aumento. A função deverá, ao final, imprimir
 # o novo valor do salário e o valor do aumento.
 
-def aumento():
-    atual = int(input("Salário atual: "))
-    porcentagem = int(input("Pontos percentuais do aumento: "))
-    
-    aumento = (porcentagem / 100 )
-    
-    valorAumento = (atual * aumento)
-    
-    novo = (atual + valorAumento)
-    
-    print ("O valor do novo salário é {} reais, e o valor do aumento foi de {} reais".format(novo, valorAumento))
 
-aumento()
+atual = int(input("Salário atual: "))
+
+porcentagem = int(input("Pontos percentuais do aumento: "))/100
+
+print ("O valor do novo salário é {} reais, e o valor do aumento foi de {} reais".format(atual + atual*porcentagem, atual*porcentagem))
+
