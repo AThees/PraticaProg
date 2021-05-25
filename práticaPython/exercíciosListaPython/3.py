216746abbaf56d7ec68e33d18162654abffc0de3
AThees <alvaro@thees.com.br>
2021-05-25 12:03:53 -0300

    Update 3.py

---
+++ práticaPython/exercíciosListaPython/3.py
@@ -0,0 +1,7 @@
+# Faça uma função que converta uma quantidade de horas digitadas pelo usuário em minutos e retorne
+# esta quantidade. Faça a leitura da quantidade de horas na função principal.
+
+horas = int(input("Número de horas: "))
+minutos = horas*60
+
+print("Em {} horas existem {} minutos".format(horas, minutos))
