4bbe04999eef4b079a70f27c2b31b3c7d8c88238
AThees <alvaro@thees.com.br>
2021-05-25 12:03:29 -0300

    Create mediaNotas.c

---
+++ práticaC/mediaNotas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x1, x2, media;
+
+    printf("Insira x1: ");
+    scanf("%d", &x1);
+
+    printf("Insira x2: ");
+    scanf("%d", &x2);
+
+    media = (x1 + x2)/2;
+
+    printf("resultado = %d", media);
+
+}
