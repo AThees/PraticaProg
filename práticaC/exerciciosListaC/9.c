8ac88f9030ef1f06023d0ed0970b8ab1b38df541
AThees <alvaro@thees.com.br>
2021-05-25 14:51:31 -0300

    Create 9.c

---
+++ práticaC/exerciciosListaC/9.c
@@ -0,0 +1,3 @@
+// Faça uma função que receba um número inteiro e teste se o valor lido termina com 0 (divisível por 10).
+// Em caso positivo, exiba a metade deste número. Caso contrário, exibir a mensagem "O número digitado
+// não termina com 0".
