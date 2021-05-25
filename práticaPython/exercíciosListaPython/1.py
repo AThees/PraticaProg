a108f4b13aeef2f6851432b749086d360e5b861f
AThees <alvaro@thees.com.br>
2021-05-25 12:03:44 -0300

    Update 1.py

--- práticaPython/exercíciosListaPython/1.py
+++ práticaPython/exercíciosListaPython/1.py
@@ -1,9 +1,7 @@
 # Faça uma função que retorne a soma de dois números reais passados como parâmetro.
 
-def soma():
-    n1 = int(input("Insira o primeiro valor:" ))
-    n2 = int(input("Insira o segundo valor:" ))
+
+n1 = int(input("Insira o primeiro valor:" ))
+n2 = int(input("Insira o segundo valor:" ))
     
-    print (n1 + n2)
-    
-soma()
+print (n1 + n2)
