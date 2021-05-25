df3bef7dcd690bf879f6ed39732046c1a10935af
AThees <alvaro@thees.com.br>
2021-05-25 14:51:25 -0300

    Create 7.c

---
+++ práticaC/exerciciosListaC/7.c
@@ -0,0 +1,3 @@
+// Faça uma função que receba como parâmetro uma nota de uma aluno e, se o valor for maior ou igual a
+// 60, imprimir na tela "APROVADO", se for menor, imprimir "REPROVADO". Testar ainda se o valor lido foi
+// maior do que 100 ou menor do que zero. Neste caso, imprimir "NOTA INVÁLIDA".
