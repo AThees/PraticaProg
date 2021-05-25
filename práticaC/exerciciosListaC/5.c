ae74b5df4891c497c768224b0515d3432a61f001
AThees <alvaro@thees.com.br>
2021-05-25 14:51:18 -0300

    Create 5.c

---
+++ práticaC/exerciciosListaC/5.c
@@ -0,0 +1,3 @@
+// Faça uma função que receba a quantidade de quilowatts consumido em uma casa e calcule o valor a ser
+// pago de energia elétrica, sabendo-se que o valor a pagar por quilowatt é de R$ 0,46. Apresentar o valor
+// total a ser pago pelo usuário acrescido de 18% de ICMS.
