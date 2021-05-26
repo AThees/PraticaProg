2d8c24c821c3cc9e1d435b8689c802e350dd84b9
AThees <alvaro@thees.com.br>
2021-05-25 23:00:11 -0300

    Create desafioLosango2.c

---
+++ DCC120/aula1/desafioLosango2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("*1111 1111*\n");
+    printf("*111   111*\n");
+    printf("*11     11*\n");
+    printf("*1       1*\n");
+    printf("*11     11*\n");
+    printf("*111   111*\n");
+    printf("*1111 1111*\n");
+
+    return 0;
+}
