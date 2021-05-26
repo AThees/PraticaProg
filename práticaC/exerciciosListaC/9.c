a5d7c384cb1f4853220ab8815fa4ee559719b556
AThees <alvaro@thees.com.br>
2021-05-25 23:01:41 -0300

    Update 9.c

--- práticaC/exerciciosListaC/9.c
+++ práticaC/exerciciosListaC/9.c
@@ -1,3 +1,21 @@
 // Faça uma função que receba um número inteiro e teste se o valor lido termina com 0 (divisível por 10).
 // Em caso positivo, exiba a metade deste número. Caso contrário, exibir a mensagem "O número digitado
 // não termina com 0".
+
+#include <stdio.h>
+
+int main(void){
+
+    int num;
+
+    printf("Insira um numero: ");
+    scanf("%d",&num);
+
+    if (num % 10 == 0)
+        printf("%d",num/2);
+
+    else 
+        printf("essse numero nao termina com 0");
+
+    return 0;
+}
