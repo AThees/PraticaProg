44b7f24a23afbfcf672017c88018d6e3e9cea3bb
AThees <alvaro@thees.com.br>
2021-05-25 23:01:36 -0300

    Update 8.c

--- práticaC/exerciciosListaC/8.c
+++ práticaC/exerciciosListaC/8.c
@@ -1,1 +1,19 @@
 // Faça uma função que receba um número inteiro e imprima o número lido é par ou impar.
+
+#include <stdio.h>
+
+int main(void){
+
+    int num;
+    printf("Insira um numero: ");
+    scanf("%d", &num);
+    
+    if (num % 2 == 0)
+
+        printf("O numero %d e par", num);
+    
+    else 
+            printf("O numero %d e impar", num);
+
+    return 0;
+}
