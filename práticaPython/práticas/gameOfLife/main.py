133136668e030f7d150fd359a32c96b2ca56b107
AThees <alvaro@thees.com.br>
2021-05-28 16:22:14 -0300

    Update main.py

---
+++ práticaPython/práticas/gameOfLife/main.py
@@ -0,0 +1,10 @@
+# Qualquer célula viva com menos de 2 vizinhos vivos morre
+# Qualquer célula viva com 2 ou 3 vizinhos continua viva
+# Qualquer célula com mais de 3 vizinhos morre
+# Qualquer célula morta com exatamente 3 vizinhos vive
+
+
+import pygame 
+
+def main():
+    
