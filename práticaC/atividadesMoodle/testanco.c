b277761926535f27c5d657d050d7b1c9ef11700d
AThees <alvaro@thees.com.br>
2021-08-02 20:03:39 -0300

    updt

--- práticaC/atividadesMoodle/testanco.c
+++ práticaC/atividadesMoodle/testanco.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
-int main(void){
-    int a = 6;
 
-    printf("%2.d", a);
+int main(void){
+
+    int num;
+
+    scanf("%d", &num);
+
+    while (1 == 1){
+
+        if(num % 2 == 0){
+
+            num = num / 2;
+
+            printf("%d\n", num);
+
+        }
+
+        else{
+
+            num = (num * 3) + 1;
+
+            printf("%d\n", num);
+
+        }
+
+    }
+
+
+return 0;
 }
