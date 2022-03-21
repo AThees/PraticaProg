df20392ab37ccc2dc39b56465af4bdda0efc0d2d
Álvaro Thees <42127686+AThees@users.noreply.github.com>
2022-03-21 19:36:42 -0300

    Delete práticaPython directory

--- práticaPython/exercíciosListaPython/1.py
+++
@@ -1,7 +0,0 @@
-# Faça uma função que retorne a soma de dois números reais passados como parâmetro.
-
-
-n1 = int(input("Insira o primeiro valor:" ))
-n2 = int(input("Insira o segundo valor:" ))
-
-print (n1 + n2)
--- práticaPython/práticas/Bots/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-while True:
-    if data_e_hora_em_texto == '10/06/2021 12:28':
-        print("agora")
-        break
